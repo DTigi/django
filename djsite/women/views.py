45d55b416bfc6e5d30d689e61a8faa3724961422
DTigi <dtiginyanu@gmail.com>
2023-10-29 16:52:05 +0300

    add functions in views.py add urls.py

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request): # HttpRequest
+    return HttpResponse('Страница приложения women')
+
+
+def categories(request):
+    return HttpResponse('<h1>статьи по категориям</h1')
