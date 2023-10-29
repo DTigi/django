8c5239a7c7462d4b6bc1e1f891cbe59e5e9ae589
DTigi <dtiginyanu@gmail.com>
2023-10-30 02:07:46 +0300

    add redirect-function

--- djsite/women/urls.py
+++ djsite/women/urls.py
@@ -4,8 +4,8 @@ from . import views, converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index), # http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories), # http://127.0.0.1:8000/cats/1/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug), # http://127.0.0.1:8000/cats/dgdff84fjg/
-    path('archive/<year4:year>/', views.archive),  # http://127.0.0.1:8000/archive/2023/
+    path('', views.index, name='home'), # http://127.0.0.1:8000/
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'), # http://127.0.0.1:8000/cats/1/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'), # http://127.0.0.1:8000/cats/dgdff84fjg/
+    path('archive/<year4:year>/', views.archive, name='archive'),  # http://127.0.0.1:8000/archive/2023/
 ]
