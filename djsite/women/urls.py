8f026b4f7799fa8f0bdc00aab0028bc741fc3777
DTigi <dtiginyanu@gmail.com>
2023-10-31 00:35:30 +0300

    update templates add variables in templates add blog. add style

--- djsite/women/urls.py
+++ djsite/women/urls.py
@@ -5,6 +5,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index, name='home'), # http://127.0.0.1:8000/
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
     path('cats/<int:cat_id>/', views.categories, name='cats_id'), # http://127.0.0.1:8000/cats/1/
     path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'), # http://127.0.0.1:8000/cats/dgdff84fjg/
     path('archive/<year4:year>/', views.archive, name='archive'),  # http://127.0.0.1:8000/archive/2023/
--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.template.loader import render_to_string
 from django.urls import reverse
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
 def index(request): # HttpRequest
-    return HttpResponse('Страница приложения women')
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+            }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request): # HttpRequest
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
+
+def blog(request): # HttpRequest
+    return render(request, 'women/blog.html')
 
 
 def categories(request, cat_id):
