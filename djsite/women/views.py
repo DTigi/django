f715060d3a67c7aec6f7bc327a77494e7c237f57
DTigi <dtiginyanu@gmail.com>
2023-11-06 12:50:40 +0300

    update

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -12,19 +12,30 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 
 data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
 ]
 
 
 def index(request): # HttpRequest
     # t = render_to_string('women/index.html')
     # return HttpResponse(t)
-    data = {'title': 'Главная страница',
-            'menu': menu,
-            'posts': data_db,
-            }
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': 0,  # не обязательная строчка
+    }
+
     return render(request, 'women/index.html', context=data)
 
 
@@ -50,6 +61,17 @@ def login(request):
 
 def blog(request): # HttpRequest
     return render(request, 'women/blog.html')
+
+
+def show_category(request, cat_id):
+    data = {
+        'title': 'Отображение по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=data)
 
 
 def page_not_found(request, exception):
