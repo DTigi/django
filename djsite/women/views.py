2ecbce503fe9dbd8722a2657f4c663d1efb7115a
DTigi <dtiginyanu@gmail.com>
2023-11-12 20:24:38 +0300

    Women model update add post and other updates

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.template.defaultfilters import slugify
+
+from women.models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -29,10 +31,11 @@ cats_db = [
 def index(request): # HttpRequest
     # t = render_to_string('women/index.html')
     # return HttpResponse(t)
+    posts = Women.objects.filter(is_published=True)
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,  # не обязательная строчка
     }
 
@@ -43,8 +46,17 @@ def about(request): # HttpRequest
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
