31b8af4dbf45269d5e946a1325919da0ed49871f
DTigi <dtiginyanu@gmail.com>
2024-01-24 23:17:03 +0300

    Введение в пагинацию. Класс Paginator. Использование на примере фунции представления "about"

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.template.loader import render_to_string
@@ -26,15 +27,13 @@ class WomenHome(DataMixin, ListView):
 
 
 def about(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            fp = UploadFile(file=form.cleaned_data['file'])
-            fp.save()
-    else:
-        form = UploadFileForm()
+    contact_list = Women.published.all()
+    paginator = Paginator(contact_list, 3)
 
-    return render(request, 'women/about.html', {'title': 'О сайте', 'form': form})
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'women/about.html', {'title': 'О сайте', 'page_obj': page_obj})
 
 
 class ShowPost(DataMixin, DetailView):
