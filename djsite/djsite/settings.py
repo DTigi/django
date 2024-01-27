b54ca022f0c668b02381cd96298b38ec4b0a68d6
DTigi <dtiginyanu@gmail.com>
2024-01-27 15:52:41 +0300

    Классы LoginView, LogoutView и AuthenticationForm Параметр next

--- djsite/djsite/settings.py
+++ djsite/djsite/settings.py
@@ -133,6 +133,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
