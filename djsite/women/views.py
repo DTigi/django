2ba3924ad93afa4c1c297067df24a8cf96bddb8e
DTigi <dtiginyanu@gmail.com>
2024-02-13 13:10:30 +0300

    Django REST Framework Класс ModelSerializer и представление ListCreateAPIView Представления UpdateAPIView и RetrieveUpdateDestroyAPIView

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -9,27 +9,39 @@ from django.template.loader import render_to_string
 from django.urls import reverse, reverse_lazy
 from django.template.defaultfilters import slugify
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    FormView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.core.cache import cache
+from rest_framework import generics
 
 from women.forms import AddPostForm, UploadFileForm, ContactForm
 from women.models import Women, Category, TagPost, UploadFile
+from women.serializers import WomenSerializer
 from women.utils import DataMixin
 
 
 class WomenHome(DataMixin, ListView):
-    template_name = 'women/index.html'
-    context_object_name = 'posts'
+    template_name = "women/index.html"
+    context_object_name = "posts"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        return self.get_mixin_context(super().get_context_data(**kwargs), title='Главная страница', cat_selected=0)
+        return self.get_mixin_context(
+            super().get_context_data(**kwargs), title="Главная страница", cat_selected=0
+        )
 
     def get_queryset(self):
-        w_lst = cache.get('women_posts')
+        w_lst = cache.get("women_posts")
         if not w_lst:
-            w_lst = Women.published.all().select_related('cat')
-            cache.set('women_posts', w_lst, 60)
+            w_lst = Women.published.all().select_related("cat")
+            cache.set("women_posts", w_lst, 60)
 
         return w_lst
 
@@ -39,21 +51,23 @@ def about(request):
     contact_list = Women.published.all()
     paginator = Paginator(contact_list, 3)
 
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'women/about.html', {'title': 'О сайте', 'page_obj': page_obj})
+    return render(
+        request, "women/about.html", {"title": "О сайте", "page_obj": page_obj}
+    )
 
 
 class ShowPost(DataMixin, DetailView):
     model = Women
-    template_name = 'women/post.html'
-    slug_url_kwarg = 'post_slug'
-    context_object_name = 'post'
+    template_name = "women/post.html"
+    slug_url_kwarg = "post_slug"
+    context_object_name = "post"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        return self.get_mixin_context(context, title=context['post'])
+        return self.get_mixin_context(context, title=context["post"])
 
     def get_object(self, queryset=None):
         return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
@@ -63,39 +77,39 @@ class AddPage(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView
     # model = Women
     # fields = ['title', 'slug', 'content', 'is_published', 'cat']
     form_class = AddPostForm
-    template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home')
-    title_page = 'Добавление статьи'
+    template_name = "women/addpage.html"
+    success_url = reverse_lazy("home")
+    title_page = "Добавление статьи"
 
     def form_valid(self, form):
         w = form.save(commit=False)
         w.author = self.request.user
         return super().form_valid(form)
 
-    permission_required = 'women.add_women'
+    permission_required = "women.add_women"
 
 
 class UpdatePage(PermissionRequiredMixin, DataMixin, UpdateView):
     model = Women
-    fields = ['title', 'content', 'photo', 'is_published', 'cat']
-    template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home')
-    title_page = 'Редактирование статьи'
+    fields = ["title", "content", "photo", "is_published", "cat"]
+    template_name = "women/addpage.html"
+    success_url = reverse_lazy("home")
+    title_page = "Редактирование статьи"
 
-    permission_required = 'women.change_women'
+    permission_required = "women.change_women"
 
 
 class DeletePage(DataMixin, DeleteView):
     model = Women
-    template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home')
-    title_page = 'Удаление статьи'
+    template_name = "women/addpage.html"
+    success_url = reverse_lazy("home")
+    title_page = "Удаление статьи"
 
 
 class ContactFormView(LoginRequiredMixin, DataMixin, FormView):
     form_class = ContactForm
-    template_name = 'women/contact.html'
-    success_url = reverse_lazy('home')
+    template_name = "women/contact.html"
+    success_url = reverse_lazy("home")
     title_page = "Обратная связь"
 
     def form_valid(self, form):
@@ -107,39 +121,61 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def blog(request): # HttpRequest
-    return render(request, 'women/blog.html')
+def blog(request):  # HttpRequest
+    return render(request, "women/blog.html")
 
 
 class WomenCategory(DataMixin, ListView):
-    template_name = 'women/index.html'
-    context_object_name = 'posts'
+    template_name = "women/index.html"
+    context_object_name = "posts"
     allow_empty = False
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cat = context['posts'][0].cat
-        return self.get_mixin_context(context, title='Категория - ' + cat.name, cat_selected=cat.id)
+        cat = context["posts"][0].cat
+        return self.get_mixin_context(
+            context, title="Категория - " + cat.name, cat_selected=cat.id
+        )
 
     def get_queryset(self):
-        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
+        return Women.published.filter(cat__slug=self.kwargs["cat_slug"]).select_related(
+            "cat"
+        )
 
 
 class TagPostList(DataMixin, ListView):
-    template_name = 'women/index.html'
-    context_object_name = 'posts'
+    template_name = "women/index.html"
+    context_object_name = "posts"
     allow_empty = False
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
-        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
+        tag = TagPost.objects.get(slug=self.kwargs["tag_slug"])
+        return self.get_mixin_context(context, title="Тег: " + tag.tag)
 
     def get_queryset(self):
-        return Women.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
+        return Women.published.filter(
+            tags__slug=self.kwargs["tag_slug"]
+        ).select_related("cat")
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
+# ____________________API_____________________________________________________________________
+
+
+class WomenAPIView(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
