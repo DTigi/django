32ad0095d9ae0df223d47136b2c4dd39d563427e
DTigi <dtiginyanu@gmail.com>
2024-02-14 17:05:49 +0300

    Авторизация на основе сессий (session-based) Реализация session-based аутентификации в DRF

--- djsite/women/urls.py
+++ djsite/women/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("edit/<slug:slug>/", views.UpdatePage.as_view(), name="edit_page"),
     path("delete/<slug:slug>/", views.DeletePage.as_view(), name="edit_page"),
     # path("api/v1/", include(router.urls)),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path("api/v1/women/", views.WomenAPIList.as_view()),
     path("api/v1/women/<int:pk>/", views.WomenAPIUpdate.as_view()),
     path("api/v1/womendelete/<int:pk>/", views.WomenAPIDelete.as_view()),
